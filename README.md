bf49b8dccb1bb118941ff9a03ef3e6a3e2760d62
Greg <gmengler@gmail.com>
2018-03-31 15:55:54 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Random Quote Generator
+
+Intermediate front-end development freeCodeCamp project.
+
+## Technologies Used
+
+* JavaScript
+* jQuery
+* AJAX
+* HTML
+* CSS
+* Bootstrap
+* Mashape random quote API
---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#getQuote").click(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+      dataType: "json",
+      success: function(data) {
+        console.log(data);
+      },
+      error: function() { alert('boo!'); },
+      beforeSend: setHeader
+    });
+
+    function setHeader(xhr) {
+      xhr.setRequestHeader("X-Mashape-Key", "pmTSpn6I45mshpuPkHokTQ01lAo1p1ugEH1jsnoOS19Gk3KQvB");
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.setRequestHeader("Accept", "application/json");
+    }
+  });
+});
