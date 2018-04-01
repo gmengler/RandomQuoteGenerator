ccdcecfcb6a1afd3983a2887b94e4b90e51d9e29
Greg <gmengler@gmail.com>
2018-04-01 15:47:04 -0600

    mvp working

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
 $(document).ready(function() {
-  $("#getQuote").click(function(e) {
-    e.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
-      dataType: "json",
-      success: function(data) {
-        console.log(data);
-
-        var $quote = $('<p>').text(data.quote);
-        var $author = $('<h3>').text(data.author);
-
-        $('.quote')
-          .append($quote)
-          .append($author);
-
-      },
-      error: function() { alert('boo!'); },
-      beforeSend: setHeader
-    });
-
-    function setHeader(xhr) {
-      xhr.setRequestHeader("X-Mashape-Key", "pmTSpn6I45mshpuPkHokTQ01lAo1p1ugEH1jsnoOS19Gk3KQvB");
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xhr.setRequestHeader("Accept", "application/json");
-    }
+  // Loads quote on page load
+  $.ajax({
+    type: "POST",
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+    responseType: "json",
+    success: function(response) {
+      showQuote(response);
+    },
+    error: function() {
+      alert('Error retreiving quote');
+    },
+    beforeSend: setHeader
   });
+
+  function setHeader(xhr) {
+    xhr.setRequestHeader("X-Mashape-Key", "pmTSpn6I45mshpuPkHokTQ01lAo1p1ugEH1jsnoOS19Gk3KQvB");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Accept", "application/json");
+  }
+
+  $("#getQuote").click(getQuote);
 });
+
+function getQuote() {
+  $.ajax({
+    type: "POST",
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+    responseType: "json",
+    success: function(response) {
+      showQuote(response);
+    },
+    error: function() {
+      alert('Error retreiving quote');
+    },
+    beforeSend: setHeader
+  });
+
+  function setHeader(xhr) {
+    xhr.setRequestHeader("X-Mashape-Key", "pmTSpn6I45mshpuPkHokTQ01lAo1p1ugEH1jsnoOS19Gk3KQvB");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Accept", "application/json");
+  }
+}
+
+function showQuote(response) {
+  console.log(response);
+
+  $('#quote').text(response.quote);
+  $('#author').text(response.author);
+}
